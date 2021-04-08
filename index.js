055777aaca2220878a0b5fffd2cc5161de1c47e7
Rahul Nair <rnair1607@gmail.com>
2021-04-08 23:55:58 +0530

    fixed cors issue

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
-const { ApolloServer, PunSub, PubSub } = require("apollo-server");
+const { ApolloServer, PubSub } = require("apollo-server");
 const mongoose = require("mongoose");
+const cors = require("cors");
+const express = require("express");
 
 const { MONGODB } = require("./config");
 const typeDefs = require("./graphql/typeDefs");
 const resolvers = require("./graphql/resolvers");
 
 const pubsub = new PubSub();
+const app = express();
+
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true,
+};
 
 const server = new ApolloServer({
+  cors: true,
   typeDefs,
   resolvers,
   context: ({ req }) => ({ req, pubsub }),
 });
+// server.applyMiddleware({ cors: false });
+
+// app.use(cors(corsOptions));
 
 mongoose
   .connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
